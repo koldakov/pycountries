f9cef3181c86dbcf2c968172b5de27ac14e77c5c
Ivan Koldakov <ivan@koldakov.com>
2024-03-10 19:43:06 +0100

    Add project info to lib

--- src/pycountries/__init__.py
+++ src/pycountries/__init__.py
@@ -1,6 +1,8 @@
 from pycountries.countries import Country
 from pycountries.currencies import Currency
 
+__version__ = "0.0.6"
+__author__ = "Ivan Koldakov"
 __all__ = [
     "Country",
     "Currency",
