35f0e1714741fe547eb1c9aea9c037954e64e057
Ivan Koldakov <ivan@koldakov.com>
2024-03-10 19:46:40 +0100

    Bump new version

--- src/pycountries/__init__.py
+++ src/pycountries/__init__.py
@@ -1,7 +1,7 @@
 from pycountries.countries import Country
 from pycountries.currencies import Currency
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "Ivan Koldakov"
 __all__ = [
     "Country",
